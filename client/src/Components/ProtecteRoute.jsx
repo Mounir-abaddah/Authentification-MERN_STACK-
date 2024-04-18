886db74da7f4466940ae04c3b8ff7d909f50a953
MOUNIR-V2 <mounirabaddah6@gmail.com>
2024-04-18 13:25:54 +0100

    Authentification(2/2)

---
+++ client/src/Components/ProtecteRoute.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+// import {Navigate} from 'react-router-dom'
+
+// const ProtecteRoute = (props) => {
+//     if(localStorage.getItem('token')){
+//         return props.children
+//     }else{
+//         return <Navigate to='/login' />
+//     }
+// }
+
+// export default ProtecteRoute
